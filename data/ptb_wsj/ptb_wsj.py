718adc97e11e2f31a1ddc03b9e283fae5ee7016c
Hiroki Teranishi <hiroki.teranishi@riken.jp>
2024-06-22 19:11:56 +0900

    Add dataset loading script.

---
+++ data/ptb_wsj/ptb_wsj.py
@@ -0,0 +1,170 @@
+import itertools
+import os
+from dataclasses import dataclass
+from typing import Optional
+
+import datasets
+from datasets.data_files import DataFilesDict, sanitize_patterns
+
+
+@dataclass
+class PtbWsjConfig(datasets.BuilderConfig):
+    encoding: str = "utf-8"
+    encoding_errors: Optional[str] = None
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.data_files is None:
+            base_path = os.path.expanduser(self.data_dir) if self.data_dir else "data/ptb_wsj"
+            base_path = os.path.abspath(base_path)
+            self.data_files = DataFilesDict.from_local_or_remote(
+                sanitize_patterns(
+                    {
+                        datasets.Split.TRAIN: "train.conll",
+                        datasets.Split.VALIDATION: "validation.conll",
+                        datasets.Split.TEST: "test.conll",
+                    }
+                ),
+                base_path=base_path,
+            )
+
+
+class PtbWsj(datasets.GeneratorBasedBuilder):
+    BUILDER_CONFIG_CLASS = PtbWsjConfig
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            features=datasets.Features(
+                {
+                    "sentence_id": datasets.Value("string"),
+                    # CoNLL-X format: https://aclanthology.org/W06-2920/
+                    "id": datasets.Sequence(datasets.Value("int32")),
+                    "form": datasets.Sequence(datasets.Value("string")),
+                    "lemma": datasets.Sequence(datasets.Value("string")),
+                    "cpostag": datasets.Sequence(datasets.Value("string")),
+                    "postag": datasets.Sequence(datasets.Value("string")),
+                    "feats": datasets.Sequence(datasets.Value("string")),
+                    "head": datasets.Sequence(datasets.Value("int32")),
+                    "deprel": datasets.Sequence(
+                        datasets.features.ClassLabel(names=_STANFORD_TYPED_DEPENDENCIES)
+                    ),
+                }
+            )
+        )
+
+    def _split_generators(self, dl_manager):
+        # https://github.com/huggingface/datasets/blob/2.20.0/src/datasets/packaged_modules/text/text.py#L46
+        if not self.config.data_files:
+            raise ValueError(
+                f"At least one data file must be specified, but got data_files={self.config.data_files}"  # noqa: E501
+            )
+        dl_manager.download_config.extract_on_the_fly = True
+        data_files = dl_manager.download_and_extract(self.config.data_files)
+        splits = []
+        for split_name, files in data_files.items():
+            if isinstance(files, str):
+                files = [files]
+            files = [dl_manager.iter_files(file) for file in files]
+            splits.append(datasets.SplitGenerator(name=split_name, gen_kwargs={"files": files}))
+        return splits
+
+    def _generate_examples(self, files):
+        idx = 0
+        for file in itertools.chain.from_iterable(files):
+            with open(
+                file, encoding=self.config.encoding, errors=self.config.encoding_errors
+            ) as f:
+                for i, tokens in enumerate(_read_conll(f)):
+                    yield (
+                        idx,
+                        {
+                            "sentence_id": f"{os.path.basename(file)}:{i}",
+                            "id": [int(token[0]) for token in tokens],
+                            "form": [token[1] for token in tokens],
+                            "lemma": [token[2] for token in tokens],
+                            "cpostag": [token[3] for token in tokens],
+                            "postag": [token[4] for token in tokens],
+                            "feats": [token[5] for token in tokens],
+                            "head": [int(token[6]) for token in tokens],
+                            "deprel": [token[7] for token in tokens],
+                        },
+                    )
+                    idx += 1
+
+
+def _read_conll(lines):
+    tokens = []
+    for line in lines:
+        line = line.strip()
+        if not line:
+            if tokens:
+                yield tokens
+                tokens = []
+        elif line.startswith("#"):
+            continue
+        else:
+            tokens.append(line.split("\t"))
+    if len(tokens) > 1:
+        yield tokens
+
+
+# Stanford typed dependencies (ver 3.3.0)
+# https://github.com/stanfordnlp/CoreNLP/blob/v3.3.0/doc/lexparser/StanfordDependenciesManual.tex
+_STANFORD_TYPED_DEPENDENCIES = [
+    "acomp",
+    "advcl",
+    "advmod",
+    "agent",
+    "amod",
+    "appos",
+    "arg",
+    "aux",
+    "auxpass",
+    "cc",
+    "ccomp",
+    "comp",
+    "conj",
+    "cop",
+    "csubj",
+    "csubjpass",
+    "dep",
+    "det",
+    "discourse",
+    "dobj",
+    "expl",
+    "goeswith",
+    "infmod",
+    "iobj",
+    "mark",
+    "mod",
+    "mwe",
+    "neg",
+    "nn",
+    "npadvmod",
+    "nsubj",
+    "nsubjpass",
+    "num",
+    "number",
+    "obj",
+    "parataxis",
+    "partmod",
+    "pcomp",
+    "pobj",
+    "poss",
+    "possessive",
+    "preconj",
+    "predet",
+    "prep",
+    "prepc",
+    "prt",
+    "punct",
+    "quantmod",
+    "rcmod",
+    "ref",
+    "root",
+    "sdep",
+    "subj",
+    "tmod",
+    "xcomp",
+    "xsubj",
+]
